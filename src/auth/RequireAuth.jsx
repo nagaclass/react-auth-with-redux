05a079e0824c2f4daa3c06028e15ccc2d111a32f
nagaclass <naga@nagaclass.com>
2022-05-27 00:41:43 +0200

    remove

--- src/auth/RequireAuth.jsx
+++ src/auth/RequireAuth.jsx
@@ -2,26 +2,21 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const RequireAuth = ({ allowedRoles }) => {
-  const { isLoggedIn, roles } = useSelector(({ userAuth }) => userAuth);
+  const { isLoggedIn, roles } =
+    useSelector(({ userAuth }) => userAuth);
   const location = useLocation();
 
-  return roles?.find(role => allowedRoles?.includes(role)) ? (
+  return roles?.find(
+    role => allowedRoles?.includes(role))
+     ? (
     <Outlet />
   ) : isLoggedIn ? (
-    <Navigate to={"/unauthorized"} state={{ from: location }} replace />
+    <Navigate to={"/unauthorized"}
+     state={{ from: location }} replace />
   ) : (
-    <Navigate to={"/login"} state={{ from: location }} replace />
+    <Navigate to={"/login"} 
+    state={{ from: location }} replace />
   );
-
-  // return isLoggedIn ? (
-  //   roles.find(role => allowedRoles.includes(role)) ? (
-  //     <Navigate to={"/login"} state={{ from: location }} replace />
-  //   ) : (
-  //     <Outlet />
-  //   )
-  // ) : (
-  //   <Navigate to={"/unauthorized"} state={{ from: location }} replace />
-  // );
 };
 
 export default RequireAuth;
