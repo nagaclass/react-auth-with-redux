8af5f6b08194775b3898a76535895b9f017b91da
nagaclass <naga@nagaclass.com>
2022-05-29 03:44:51 +0200

    remove company reducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,6 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import companyReducer from "./company/company.reducer";
-import personTabsReducer from "./person-tabs/personTabs.reducer";
-import personsReducer from "./persons/persons.reducer";
-import uiReducer from "./ui/ui.redusers";
-import userReducer from "./user/user.reducer";
 import userAuthReducer from "./userAuth/userAuth.reducer";
 
 const persistConfig = {
@@ -15,11 +10,6 @@ const persistConfig = {
 };
 
 const rootReducer = combineReducers({
-  person: personsReducer,
-  ui: uiReducer,
-  company: companyReducer,
-  tabs: personTabsReducer,
-  user: userReducer,
   userAuth: userAuthReducer,
 });
 
